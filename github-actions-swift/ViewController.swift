875b11ffae068e035203fb8c18f95bed1272f79d
Gregorius Albert <galbert.djaja@gmail.com>
2022-11-01 12:05:21 +0700

    Initial Commit

---
+++ github-actions-swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  github-actions-swift
+//
+//  Created by Gregorius Albert on 01/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
